aef5a61dd3a6aab52ca7c17617e1bf4ad2754a56
RStainforth <R.Stainforth@liv.ac.uk>
2012-12-12 09:08:29 +0000

    Update intToriverse.cpp

--- intToriverse.cpp
+++ intToriverse.cpp
@@ -1,37 +1,26 @@
 
 #include "Toriverse.h"
+#include "Harvester.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
-  Toriverse testTori("toriverse_test.map");
-  cout << "X Dim is: " << testTori.getXDim() << endl;
-  cout << "Y Dim is: " << testTori.getYDim() << endl;
-  cout << "LifeTime is: " << testTori.getLifeTime() << endl;
-  cout << "Energy Density is: " << testTori.getEnergyDensity() << endl;
-  cout << "Status is: " << testTori.getStatus() << endl;
+  cout << "Welcome Player...to the Toriverse!" << endl;
+  Toriverse testTori("toriverse_01_01001.map");
+  Harvester testHarvey("toriverse_01_01001.map");
 
-  cout<< "Object at (0,0): " << testTori.getObject(0,0) << endl;
-  for(int y=0;y<testTori.getYDim();y++){
-    for(int x=0;x<testTori.getXDim();x++){
-      cout<<testTori.getObject(x,testTori.getYDim()-y-1);
-    }
-    cout << endl;
-  }
-  
-  //testTori.setObject("@",7,0);
-  cout << "NEW UNIVERSE" << endl;
 
-  for(int y=0;y<testTori.getYDim();y++){
-    for(int x=0;x<testTori.getXDim();x++){
-      cout<<testTori.getObject(x,testTori.getYDim()-y-1);
-    }
-    cout << endl;
+  while(testHarvey.getStatus()){
+    char cmd;
+    cout << "Please Enter your Command: " << endl;
+    cin >> cmd;
+    testHarvey.HarvesterCMD(testTori, cmd);
+    testTori.Interaction(testHarvey);
   }
 
-  cout << "Number of SubSpace Locks is: " << testTori.getNSLocks() << endl;
-  cout << "Position of wormhole b is: " <<testTori.getWHolePos(1,0,0) << ","<< testTori.getWHolePos(1,0,1) << "||" << testTori.getWHolePos(1,1,0) << ","<< testTori.getWHolePos(1,1,1) << endl;
+  cout << testHarvey.getFate() << endl;
+  cout << "Your Score is: " << testHarvey.getScore() << endl;
   return 0;
 }
   
